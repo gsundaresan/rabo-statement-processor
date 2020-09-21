7416a695b6b16c80ca515599b68e2e448cbac057
GuhanS <goguhan@gmail.com>
2020-09-22 02:18:37 +0530

    Refactoring

--- src/main/java/com/rabo/transactions/exception/RaboExceptionHandler.java
+++ src/main/java/com/rabo/transactions/exception/RaboExceptionHandler.java
@@ -3,6 +3,8 @@ package com.rabo.transactions.exception;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,13 +15,16 @@ import com.rabo.transactions.utils.RaboConstants;
 @RestControllerAdvice
 public class RaboExceptionHandler {
 	
+	private static final Logger LOGGER = LogManager.getLogger(RaboExceptionHandler.class);
 	@ExceptionHandler(IOException.class)
 	public ResponseEntity<ValidationResponse> handleReaderExceptions(IOException ioException){
+		LOGGER.error(ioException.getMessage(),ioException);
 		return ResponseEntity.badRequest().body(new ValidationResponse(RaboConstants.RABO_READ_EXCEPTION+ ":" + ioException.getMessage(), new ArrayList<>()));
 	}
 
 	@ExceptionHandler(RaboTransactionException.class)
 	public ResponseEntity<ValidationResponse> handleValidatorExceptions(RaboTransactionException rabException){
+		LOGGER.error(rabException.getMessage(),rabException);
 		return ResponseEntity.badRequest().body(new ValidationResponse(RaboConstants.RABO_READ_EXCEPTION+ ":" + rabException.getMessage(), new ArrayList<>()));
 	}
 }
